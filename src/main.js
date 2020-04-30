668af63e79cac82b1365ce4042590b904ce5c310
ana-ramos09 <ana.carolina.dos.reis.ramos@gmail.com>
2020-04-30 19:52:44 -0300

    alterações comentarios

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@
 
 import myObject from './data/pokemon/pokemon.js';
 
-
+// Essa função cria os mini cards e atribui uma imagem e um label para cada um deles, 
+// utilizando um looping para criar cada mini card em cada iteração;
+// Atribuindo também um id, uma classe e chama uma função com vários evento a cada mini card dinamicamente
+ 
 function createMiniCard(n) {
 
     //Essa parte cria uma DIV que será cada mini card
@@ -34,34 +37,11 @@ for (let i = 0 ; i < 151; i++) {
 	createMiniCard(i);
 }
 
+// Esse bloco de funções é o conjunto de funções de eventos que vão ocorrer no mini card
+// A função miniCardMouseEvents agrega todos as funções com cada evento unitário que está será apliacado no mini card
+// 
 
-
-function getSelectedPokemonIndex(){
-    let pokemonIndex;
-    let selectedPokemon = event.currentTarget.querySelector('textarea').innerHTML;
-    for (let i = 0 ; i < 151; i++) {
-        if (myObject.pokemon[i].name == selectedPokemon) {
-            pokemonIndex = i;
-            break;
-        }
-    }
-    pokedexLoadData(pokemonIndex);
-}
-
-
-function pokedexLoadData(pokemonIndex){
-    let pokemonTypes = "";
-    for (let i = 0 ; i < myObject.pokemon[pokemonIndex].type.length; i++) {
-        pokemonTypes += myObject.pokemon[pokemonIndex].type[i] + ", ";
-    }
-    
-}
-    
-    
-    
-
-
-//Funcao para criar eventos em cada Minicard
+// Função para criar eventos em cada mini card
 function miniCardMouseEvents(selectedDiv){
     let selectedMiniCard = document.getElementById(selectedDiv);
     selectedMiniCard.addEventListener("mouseover", minicardMouseOver);
@@ -82,4 +62,37 @@ function minicardMouseOut() {
 function minicardMouseClick() {
     getSelectedPokemonIndex();
 }
+
+// Essa função vai reconhecer qual Pokémon foi clicado
+// Ela faz isso utilizando o valor do text area (nome do Pokémon)
+// Ela utiliza o valor do text area em um looping em cada Pokémon para reconhecer o index do Pokémon clicado,
+// Após isso ela utiliza o index encontrado para carregar a Pokédex  (função pokedexLoadData)
+//
+
+function getSelectedPokemonIndex(){
+    let pokemonIndex;
+    let selectedPokemon = event.currentTarget.querySelector('textarea').innerHTML;
+    for (let i = 0 ; i < 151; i++) {
+        if (myObject.pokemon[i].name == selectedPokemon) {
+            pokemonIndex = i;
+            break;
+        }
+    }
+    pokedexLoadData(pokemonIndex);
+}
+
+// Para cada propriedade do objeto, vai ter um looping que você vai fazer para efetivamente criar e 
+// trazer as informações para a Pokedex, então você só vai adaptar o código a informação que realmente vai ser trazida na Pokedex
+// looping por cada 'type' do Pokémon, adicionando cada item
+// na variável types, e depois colocar isso no label 'pokemonType'
+
+
+function pokedexLoadData(pokemonIndex){
+    let pokemonTypes = "";
+    for (let i = 0 ; i < myObject.pokemon[pokemonIndex].type.length; i++) {
+        pokemonTypes += myObject.pokemon[pokemonIndex].type[i] + ", ";
+    }
+    
+}
+
 
